ae474f6f8f56d4f2033b59c8c6bd3d536f04c3e8
Alejandro <evergreenterras@hotmail.com>
2021-02-28 13:55:40 +0100

    funko encontrado y borrado

--- server/routes/funkos.routes.js
+++ server/routes/funkos.routes.js
@@ -9,7 +9,7 @@ router.post('/nuevoFunko', (req, res) => {
     Funko
         .create(req.body)
         .then(response => res.json(response))
-        .catch(err => res.status(500).json({ code: 500, message: 'Error saving funkos', err }))
+        .catch(err => res.status(500).json({ code: 500, message: 'Error saving funko', err }))
 })
 
 //lista de funkos -> todo ok
@@ -20,7 +20,7 @@ router.get('/listadoFunkos', (req, res) => {
         .then(response => res.json(response))
         .catch(err => res.status(500).json({ code: 500, message: 'Error fetching funkos', err }))
 })
-// //listado por filtro 
+// //listado por filtro este tengo que estudiarmelo bien
 
 
 //detalles del funko -> todo ok
@@ -29,8 +29,19 @@ router.get('/Funko/detalles/:funko_id', (req, res) => {
     Funko
         .findById(req.params.funko_id)
         .then(response => res.json(response))
-        .catch(err => res.status(500).json({ code: 500, message: 'Error fetching funkos', err }))
+        .catch(err => res.status(500).json({ code: 500, message: 'Error fetching funko', err }))
 })
+
+
+//borrar funko -> todo ok, lo encuentra y lo quita de la DB
+router.get('/Funko/borrar/:funko_id', (req, res) => {
+    
+    Funko
+    .findByIdAndDelete(req.params.funko_id)
+    .then(response => res.json(response))
+    .catch(err => res.status(500).json({ code: 500, message: 'Error deleting funko', err }))
+})
+
 
 //editar funko -> todo ok
 router.put('/Funko/editar/:funko_id', (req, res) => {
@@ -40,8 +51,5 @@ router.put('/Funko/editar/:funko_id', (req, res) => {
         .then(response => res.json(response))
         .catch(err => res.status(500).json({ code: 500, message: 'Error editing funko', err }))
 })
-
-//borrar funko
-
 
 module.exports = router
